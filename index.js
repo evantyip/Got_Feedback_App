e009a540d18c5d737a683d723c88d5484ffe70bc
Evan Yip <etyip@ucsb.edu>
2019-09-13 17:29:12 -0700

    changed greeting

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ hi: 'What up buddy' });
 });
 
 const PORT = process.env.PORT || 3000;
