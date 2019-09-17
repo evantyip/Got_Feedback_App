bef96ef4c51d4abeec4f962e0331fb8c6664dfea
Evan Yip <etyip@ucsb.edu>
2019-09-16 19:14:50 -0700

    added billing and client side

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Got Feedback?</h1>
+            Get feedback from your users
+        </div>
+    );
+};
+
+export default Landing;
